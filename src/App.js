5ecb9bc5441f9fdff743da27ac26b79e4261a593
Jawad Syed <sjahmed@rogers.com>
2020-04-02 22:55:06 -0400

    final layout 05 of site on 2nd Apr 2020

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
