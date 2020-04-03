4a3a12fdaed2a1aa1cca7c78ca721df36e7844bf
Jawad Syed <sjahmed@rogers.com>
2020-04-02 22:41:45 -0400

    Updates

---
+++ precache-manifest.db38b25e582e6bf140d9b5a0ed3c2932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f777f782d25feb5de36c6de66e4c2759",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "837ef071fe1b3bda2619",
+    "url": "/reactportfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "77f383eb154283098e53",
+    "url": "/reactportfolio/static/js/2.66463df0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactportfolio/static/js/2.66463df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "837ef071fe1b3bda2619",
+    "url": "/reactportfolio/static/js/main.21520240.chunk.js"
+  },
+  {
+    "revision": "f8ca34d2a84711bf872c",
+    "url": "/reactportfolio/static/js/runtime-main.0c1598a4.js"
+  }
+]);
